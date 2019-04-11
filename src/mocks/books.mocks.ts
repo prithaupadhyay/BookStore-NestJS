c1f870acd2a829cfd9cbf9ef5f8683fe6f5b59ec
prithaupadhyay <prithaupadhyay@gmail.com>
2019-04-11 22:09:19 +0530

    First Version done

---
+++ src/mocks/books.mocks.ts
@@ -0,0 +1,8 @@
+export const BOOKS = [
+    { id: 1, title: 'First book', description: 'This is the description for the first book', author: 'Enid Blyton' },
+    { id: 2, title: 'Second book', description: 'This is the description for the second book', author: 'Nancy Drew' },
+    { id: 3, title: 'Third book', description: 'This is the description for the third book', author: 'Charles Darwin' },
+    { id: 4, title: 'Fourth book', description: 'This is the description for the fourth book', author: 'Robert Frost' },
+    { id: 5, title: 'Fifth book', description: 'This is the description for the fifth book', author: 'Paulo Coelho' },
+    { id: 6, title: 'Sixth book', description: 'This is the description for the sixth book', author: 'Olususi Oluyemi' },
+];
